c1be149b36897fa6b54632670859465b3f59661c
Vulfka <poblete.gnicolas@gmail.com>
2026-02-08 12:03:41 -0300

    Actualización del código

--- main/src/Principal.java
+++ main/src/Principal.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) { // Agregado 'public'
-        String nombre = "Nicolas";
-        double saldo = 1599.99;
         int opcion = 0;
         Scanner teclado = new Scanner(System.in);
         Conversion conversor = new Conversion();
@@ -11,13 +9,13 @@ public class Principal {
         while (opcion != 9) {
             System.out.println("""
                 ************************************************************
-                Bienvenido/a %s al conversor de moneda
+                Bienvenido/a al conversor de moneda
                 1 - Dólar =>> Peso chileno
                 2 - Peso chileno =>> Dólar
-                3 - Consultar Saldo
+                3 - Otra conversión
                 9 - Salir
                 ************************************************************
-                """.formatted(nombre));
+                """);
 
             opcion = teclado.nextInt();
 
@@ -34,11 +32,29 @@ public class Principal {
                     System.out.println("Ingrese el monto en CLP:");
                     double montoClp = teclado.nextDouble();
                     Moneda res2 = conversor.convertirMoneda("CLP", "USD", montoClp);
+                    System.out.println("************************************************************");
                     System.out.println("Resultado: " + montoClp + " CLP son " + res2.conversion_result() + " USD");
+                    System.out.println("************************************************************\n");
                     break;
 
                 case 3:
-                    System.out.println("Su saldo actual es: $" + saldo);
+                    System.out.println("Ingrese las siglas de la moneda de origen (ej: USD, ARS, BRL):");
+                    String monedaBase = teclado.next().toUpperCase(); // Cambiado de nextString a next
+
+                    System.out.println("Ingrese las siglas de la moneda de destino (ej: CLP, EUR, MXN):");
+                    String monedaDestino = teclado.next().toUpperCase();
+
+                    System.out.println("Ingrese el monto que desea convertir:");
+                    double monto = teclado.nextDouble();
+
+                    // Invocamos el método con las variables que el usuario escribió
+                    Moneda res3 = conversor.convertirMoneda(monedaBase, monedaDestino, monto);
+
+                    System.out.println("************************************************************");
+                    System.out.println("Resultado: " + monto + " " + monedaBase + " son: "
+                            + res3.conversion_result() + " " + monedaDestino);
+                    System.out.println("Tasa de cambio aplicada: " + res3.conversion_rate());
+                    System.out.println("************************************************************\n");
                     break;
 
                 case 9:
